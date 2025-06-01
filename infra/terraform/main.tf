038fd3c477c267791d774c827dabcefee0f89dfc
RamyaLogan <31441976+RamyaLogan@users.noreply.github.com>
2025-06-01 19:02:14 -0400

    deploy ec2

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -10,20 +10,38 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
-resource "aws_lightsail_instance" "odoo" {
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2_sg"
+  description = "Allow SSH, HTTP and HTTPS"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "odoo" {
+  ami = "ami-084568db4383264d4"
+  instance_type = "t3.medium"
   name = var.instance_name
   availability_zone = "ca-central-1a"
-  blueprint_id = "ubuntu_22_04"
-  bundle_id = "small_2_0"
-  key_pair_name = var.key_pair_name
-}
-
-resource "aws_lightsail_static_ip" "odoo_ip" {
-  name = "odoo-ip"
-}
-
-resource "aws_lightsail_static_ip_attachment" "attach_ip" {
-  static_ip_name = aws_lightsail_static_ip.odoo_ip.name
-  instance_name = aws_lightsail_instance.odoo.name
+  key_name = var.key_pair_name
 }
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "odoo_server_ip" {
-  value = aws_lightsail_static_ip.odoo_ip.ip_address
+  value = aws_instance.odoo.public_ip
+}
+output "public_dns" {
+  value = aws_instance.odoo.public_dns
 }
