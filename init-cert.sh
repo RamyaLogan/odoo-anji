873a33f5f0346c20f144f096559517869bebd7e8
RamyaLogan <31441976+RamyaLogan@users.noreply.github.com>
2025-06-03 00:08:40 -0400

    Update init-cert.sh

--- init-cert.sh
+++ init-cert.sh
@@ -9,10 +9,10 @@ CERTBOT_PATH=/opt/ssl/certbot
 cp nginx/nginx-http.conf nginx/conf.d/default.conf
 docker compose down nginx && docker-compose up -d nginx
 sleep 5
-if [ ! -f "$CERTBOT_PATH/live/$DOMAIN/fullchain.pem" ]; then
-        echo "no cert found. Bootstrapping https..."
+if [ ! -f "$CERTBOT_PATH/live/mhs.doneztech.com/fullchain.pem" ]; then
+        echo "no cert found. Bootstrapping https...mhs.doneztech.com"
     
-        docker-compose run --rm certbot certbot certonly --webroot -w $WEBROOT -d $DOMAIN  --email $EMAIL --agree-tos --non-interactive
+        docker-compose run --rm certbot certbot certonly --webroot -w $WEBROOT -d mhs.doneztech.com  --email $EMAIL --agree-tos --non-interactive
                
 fi
 cp nginx/nginx-https.conf nginx/conf.d/default.conf
