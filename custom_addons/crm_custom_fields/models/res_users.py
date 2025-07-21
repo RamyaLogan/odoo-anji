efc1269d0c421f060206b95b5a1b0b35804d806a
RamyaLogan <ramyalogan30@gmail.com>
2025-07-21 01:08:15 -0400

    check

--- custom_addons/crm_custom_fields/models/res_users.py
+++ custom_addons/crm_custom_fields/models/res_users.py
@@ -4,8 +4,5 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    role_level = fields.Selection([
-        ('senior', 'Senior'),
-        ('junior', 'Junior'),
-        ('trainee', 'Trainee'),
-    ], string="Role Level")
+    user_role_id = fields.Many2one('user.role', string="User Role")
+    
