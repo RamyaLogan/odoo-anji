c9d969fe4e34a1cef09cc404929ac4a217b1c5ae
RamyaLogan <ramyalogan30@gmail.com>
2025-07-20 23:09:59 -0400

    dashboard height fix

--- custom_addons/crm_custom_fields/__manifest__.py
+++ custom_addons/crm_custom_fields/__manifest__.py
@@ -12,6 +12,8 @@
         "views/crm_lead_stage_view.xml",
         "views/crm_assign_lead_view.xml",
         "views/crm_assign_lead_wizard_view.xml",
+        "views/res_users_view.xml",
+        "views/user_role_view.xml",
     ],
     "installable": True,
     "application": False,
