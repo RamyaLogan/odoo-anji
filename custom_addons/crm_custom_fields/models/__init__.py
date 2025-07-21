b37a989e2fb554537d3e929a31f53f6a52c9a106
RamyaLogan <ramyalogan30@gmail.com>
2025-07-21 01:20:01 -0400

    check

--- custom_addons/crm_custom_fields/models/__init__.py
+++ custom_addons/crm_custom_fields/models/__init__.py
@@ -1,3 +1,1 @@
-from . import res_users
-from . import crm_call_lead
-from . import user_role
+from . import crm_call_lead,user_role
--- custom_addons/crm_custom_fields/models/res_users.py
+++ custom_addons/crm_custom_fields/models/res_users.py
@@ -5,4 +5,9 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     user_role_id = fields.Many2one('user.role', string="User Role")
-    
+    role_level = fields.Selection([
+        ('senior', 'Senior'),
+        ('junior', 'Junior'),
+        ('trainee', 'Trainee'),
+    ], string="Role Level")
+
