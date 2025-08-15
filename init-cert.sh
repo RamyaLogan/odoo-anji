79c63b7f71e0ac052d18afe194f5ddcce5599c87
RamyaLogan <ramyalogan30@gmail.com>
2025-08-15 14:10:52 -0400

    update

--- init-cert.sh
+++ init-cert.sh
@@ -10,8 +10,8 @@ for DOMAIN in "${DOMAINS[@]}"; do
     if [ ! -f "$CERTBOT_PATH/live/$DOMAIN/fullchain.pem" ]; then
         echo "no cert found. Bootstrapping https..."
     
-        docker-compose run --rm certbot certbot certonly --webroot -w $WEBROOT -d $DOMAIN   -d www.$DOMAIN \  --email $EMAIL --agree-tos --non-interactive
-               
+        docker-compose run --rm certbot certbot certonly --webroot -w $WEBROOT -d $DOMAIN   -d www.$DOMAIN   --email $EMAIL --agree-tos --non-interactive
+             
     fi
 done
 cp nginx/nginx-https.conf nginx/conf.d/default.conf
